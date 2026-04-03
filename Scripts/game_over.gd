be01b9911517a37c96b95940a41acc0c7615fa89
hujow <hugowyler@gmail.com>
2026-04-03 15:17:16 +0200

    game over refine

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -14,10 +14,12 @@ func _ready():
 	if final_score == 0:
 		msg = "You didn't type any names!"
 	else:
-		var estimate_sec = (float(total_names) * timer_duration) / float(final_score)
+		var estimate_sec = (75227 * timer_duration) / float(final_score)
 		msg = "At this speed, it would take you\n"
 		msg += format_duration(estimate_sec)
-		msg += "\nto type all the names of the victims."
+		msg += "\nto type all the names of the victims"
+		msg += "\nof the Israel Defence Force and Hamas "
+		msg += "\nsince October 7 2023."
 		
 	stats_label.text = msg
 
@@ -37,3 +39,10 @@ func format_duration(seconds_float: float) -> String:
 	sec = sec % 60
 	
 	return str(days) + " days " + str(hours) + " hours " + str(mins) + " minutes " + str(sec) + " seconds"
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+	
+func _on_learn_more_button_pressed():
+	OS.shell_open("https://en.wikipedia.org/wiki/Casualties_of_the_Gaza_war")	
